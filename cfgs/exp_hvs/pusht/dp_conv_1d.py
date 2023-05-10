b675084e9badcbfd1979901ffce4c50b6c00731c
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-05-10 15:08:30 -0700

    update

--- cfgs/exp_hvs/pusht/dp_conv_1d.py
+++ cfgs/exp_hvs/pusht/dp_conv_1d.py
@@ -10,7 +10,7 @@ from muse.utils.general_utils import dc_skip_keys
 export = dc_skip_keys(bc_rnn.export, 'model') & d(
     use_ema=False,
     horizon=16,
-    exp_name='push_t/lang_clip_posact{?use_keypoint:-kp}_{?seed:s{seed}_}b{batch_size}_h{horizon}'
+    exp_name='push_t/2_posact{?use_keypoint:-kp}_{?seed:s{seed}_}b{batch_size}_h{horizon}'
              '{?use_ema:_ema}_{dataset}{?use_norm:_norm}',
     use_norm=True,
     model=dp_conv_1d.export & d(
--- muse/models/diffusion/dp.py
+++ muse/models/diffusion/dp.py
@@ -363,7 +363,8 @@ class DiffusionPolicyModel(Model):
             )
 
         """
-
+        import pdb;pdb.set_trace()
+        
         # does normalization potentially
         if inputs is not None:
             inputs = self._preamble(inputs)

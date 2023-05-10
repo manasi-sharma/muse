ecaddf83e6af0fbb52853040e7398fe25e8229f3
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-05-09 22:32:54 -0700

    update

--- muse/models/diffusion/dp.py
+++ muse/models/diffusion/dp.py
@@ -133,6 +133,7 @@ class DiffusionPolicyModel(Model):
                 for param in self.t5_model_sentence.parameters():
                     param.requires_grad = False
                     
+                import pdb;pdb.set_trace()
                 embeddings = np.expand_dims(self.t5_model_sentence.encode(instruction), 0)
                 self.lang_repr = torch.Tensor(embeddings, device=device)
 

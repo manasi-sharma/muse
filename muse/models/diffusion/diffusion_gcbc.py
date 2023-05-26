7220578a320beb18682cf3256892d2af2f0d9d2f
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-05-25 21:56:16 -0700

    update

--- muse/models/diffusion/diffusion_gcbc.py
+++ muse/models/diffusion/diffusion_gcbc.py
@@ -242,7 +242,6 @@ class DiffusionGCBC(BaseGCBC):
             combine_then_concatenate(action_dc, self.action_decoder.action_names, dim=2).to(dtype=torch.float32)
 
         # model forward
-        import pdb;pdb.set_trace()
         model_outputs = self(inputs, **kwargs)
 
         """ 
